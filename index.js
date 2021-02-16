e4dbb6ba7c47fa260692205ba88a839b5f1ad0cb
Craig Neff <ifly12610@gmail.com>
2021-02-15 23:18:04 -0700

    started this coding assigment

---
+++ index.js
@@ -0,0 +1,9 @@
+"use strict";
+
+//a function that takes firstName and lastName and returns ‘fistName lastName’
+function nameMaker(firstName, lastName) {
+  console.log(`${firstName} ${lastName}`);
+  return `${firstName} ${lastName}`;
+}
+
+nameMaker("Craig", "Neff");
