100db73dd8629f00f094831a7d9421783650c55a
Craig Neff <ifly12610@gmail.com>
2021-02-17 00:31:36 -0700

    working on the coding assigment

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 "use strict";
-
+/*
 // 1. a function that takes firstName and lastName and returns ‘fistName lastName’
 function nameMaker(firstName, lastName) {
   console.log(`${firstName} ${lastName}`);
@@ -19,6 +19,43 @@ console.log(name12);
 function timerUp() {
   alert(`Time is up!! It as been 3 second!`);
 }
+() => setTimeout(timerUp, 300);
+// 3b. an arrow function named askAreWeThereYet that alerts ‘Are we there yet?’. Using setInterval, pass askAreWeThereYet into the callback parameter position.
 
-const holden = () => setTimeout(timerUp, 3000);
-holden();
+const askAreWeThereYet = () => alert(`Are we there yet?`);
+
+function keepAsking() {
+    setInterval(askAreWeThereYet, 2000);
+}
+
+keepAsking();
+//Wanted to try writing an arrow function as the function parameter in setInterval. SO COOL!
+function answerSon() {
+    setInterval(() => {
+        alert(`No`);
+    }, 3000);
+}
+
+answerSon();
+*/
+// 4a. New function named processSplicedValue that takes 3 parameters (array, the index of the element to be spliced from the array, and a callback function). Inside function, use the first two parameters to splice an element from array.Inside the function, call the callback function and pass the spliced value into it.Outside of the function, create an array of strings, call processSplicedValue, and pass in the array you just created, an index number, and console.log into it.
+
+var processSplicedValueList = [
+  "Batman",
+  "SuperMan",
+  "Ironman",
+  "War Machine",
+  "Black Widow",
+  "SpiderMan",
+];
+function bestHero(me) {
+  alert(`The best hero is ${me}`); //ths time with an alert call
+}
+
+function processSplicedValue(array, spliceInd, functionProcess) {
+  var hero = processSplicedValueList.splice(spliceInd, 1);
+  functionProcess(hero);
+}
+
+processSplicedValue(processSplicedValueList, 5, bestHero);
+console.log(typeof processSplicedValue);
