fdf1a1a92d0850d60a3756fade7e839cf351aa2c
Craig Neff <ifly12610@gmail.com>
2021-02-16 22:17:16 -0700

    done with 3a

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 "use strict";
-/*
+
 // 1. a function that takes firstName and lastName and returns ‘fistName lastName’
 function nameMaker(firstName, lastName) {
   console.log(`${firstName} ${lastName}`);
@@ -16,19 +16,9 @@ console.log(name12);
 
 // 3a. Using setTimeout, write an anonymous (has no name assigned to it) arrow function in the callback parameter position. The function should alert ‘Time is up!’
 
-function timerStarted() {
-  console.log(`The timer has started`);
-}
-var time = 30000;
-
-(timerStarted, time) => setTimeout(timerStarted, time), alert(`Time is up!`);
-*/
 function timerUp() {
-  alert(`The timer has ended! It as been 10 second!`);
-}
-function startTimer() {
-  console.log(`Count down started`);
-  setTimeout(timerUp, 10000);
+  alert(`Time is up!! It as been 3 second!`);
 }
 
-startTimer();
+const holden = () => setTimeout(timerUp, 3000);
+holden();
