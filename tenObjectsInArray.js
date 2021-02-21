81ed86bea3f1de354168bea72d75c93a271684c3
Craig Neff <ifly12610@gmail.com>
2021-02-21 16:20:19 -0700

    created a 10 object array for practiceing

---
+++ tenObjectsInArray.js
@@ -0,0 +1,115 @@
+"use strict";
+
+var customerInfo = [
+  {
+    name: "Sam",
+    address: {
+      street: "1234 W Bell Rd",
+      city: "Phoenix",
+      zip: "85308",
+      state: "AZ",
+    },
+    membershipLevel: "GOLD",
+    age: 32,
+  },
+  {
+    name: "Craig",
+    address: {
+      street: "24345 W Evertt ct. ",
+      city: "Florida",
+      zip: "82328",
+      state: "CO",
+    },
+    membershipLevel: "GOLD",
+    age: 35,
+  },
+  {
+    name: "Jenna",
+    address: {
+      street: "4256 West Drive Rd",
+      city: "Seattle",
+      zip: "85308",
+      state: "AZ",
+    },
+    membershipLevel: "GOLD",
+    age: 23,
+  },
+  {
+    name: "Rick",
+    address: {
+      street: "1234 W Bell Rd",
+      city: "Clarkson",
+      zip: "85308",
+      state: "OH",
+    },
+    membershipLevel: "GOLD",
+    age: 22,
+  },
+  {
+    name: "Jennifer",
+    address: {
+      street: "1234 W Bluesky Rd",
+      city: "Denver",
+      zip: "85308",
+      state: "CO",
+    },
+    membershipLevel: "GOLD",
+    age: 34,
+  },
+  {
+    name: "Holden",
+    address: {
+      street: "1234 W Bluesky Rd",
+      city: "Denver",
+      zip: "85308",
+      state: "CO",
+    },
+    membershipLevel: "GOLD",
+    age: 4,
+  },
+  {
+    name: "Lisa",
+    address: {
+      street: "1234 W Bluesky Rd",
+      city: "Pine",
+      zip: "85308",
+      state: "CO",
+    },
+    membershipLevel: "GOLD",
+    age: 44,
+  },
+  {
+    name: "Matt",
+    address: {
+      street: "1234 W Brooke Rd",
+      city: "Denver",
+      zip: "85308",
+      state: "CO",
+    },
+    membershipLevel: "GOLD",
+    age: 24,
+  },
+  {
+    name: "Holden",
+    address: {
+      street: "1234 W Bluesky Rd",
+      city: "Denver",
+      zip: "85308",
+      state: "CO",
+    },
+    membershipLevel: "GOLD",
+    age: 23,
+  },
+  {
+    name: "Jesse",
+    address: {
+      street: "1234 W Happy Rd",
+      city: "bfe",
+      zip: "85308",
+      state: "GA",
+    },
+    membershipLevel: "GOLD",
+    age: 54,
+  },
+];
+console.log(customerInfo[0].name);
